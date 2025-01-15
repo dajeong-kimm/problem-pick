9697b74f687bcef4e2374c95fdf265399b1a34c5
DajeongKim <rlaekwjd6566@gmail.com>
2025-01-15 14:26:51 +0900

    feat: handle 추가

--- pick/src/main/java/com/pick/repository/UserRepository.java
+++ pick/src/main/java/com/pick/repository/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 public class UserRepository {
     public List<String> getAllUsers() {
         // Replace with actual user data source
-        return Arrays.asList("rlaekwjd6545", "3957ki", "phs7646", "binaryarc", "dlsxj101", "wnsgh712", "hyunddoing", "gmltjd8002", "shbak111", "kwonseon0827", "ejoy_1016");
+        return Arrays.asList("rlaekwjd6545", "3957ki", "phs7646", "binaryarc",
+                "dlsxj101", "wnsgh712", "hyunddoing", "gmltjd8002", "shbak111",
+                "kwonseon0827", "ejoy_1016", "ann0460", "95dmstjs");
     }
 }
